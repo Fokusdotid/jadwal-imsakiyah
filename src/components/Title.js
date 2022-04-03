015eef0ee04f1cfe32c8110dcc92ff0ab770847d
FokusDotId <mr.familia13@gmail.com>
2022-04-03 08:52:25 +0700

    update

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,11 +3,10 @@ import React from "react";
 function Title() {
   return (
     <div className="p-3 text-center ">
-      <h1 className="text-white">Jadwal</h1>
-      <h1 className="text-white">Imsakiyah</h1>
-      <h2 className="text-white">Kabupaten Grobogan</h2>
+      <h1 className="text-white">Jadwal Imsakiyah</h1>
+      <h1 className="text-white">Kabupaten Grobogan</h1>
       <h2 className="text-white">Ramadhan 1443 H/2022 M</h2>
-      <h3 className="text-white">By Aguz Familia</h3>
+      <h2 className="text-white">WebSite By Aguz Familia</h2>
     </div>
   );
 }
