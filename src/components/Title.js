b78dc1af34ce133f49b32ea878c5f7a868a61df2
FokusDotId <mr.familia13@gmail.com>
2022-04-03 08:45:17 +0700

    update

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,6 +7,7 @@ function Title() {
       <h1 className="text-white">Imsakiyah</h1>
       <h2 className="text-white">Kabupaten Grobogan</h2>
       <h2 className="text-white">Ramadhan 1443 H/2022 M</h2>
+      <h3 className="text-white">By Aguz Familia</h3>
     </div>
   );
 }
